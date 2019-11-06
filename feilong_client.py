be73437f6a8941e7bec9a6d9489d2c8fe324757f
56KbModem <nick-snel@hotmail.com>
2019-11-06 16:01:20 +0100

    JSON POST requests implemented

--- feilong_client.py
+++ feilong_client.py
@@ -81,7 +81,6 @@ def send_post_request():
 	api_name = input("Please provide the API name: ")
 	request_url = 'http://{0}:{1}/{2}'.format(feilong_server, feilong_port, api_name)
 	
-
 	# Read the data for POST request
 	# Open a file with the same api name
 	# ending with the suffix '.json'
@@ -92,15 +91,34 @@ def send_post_request():
 	editing = 1   # do we want to edit the request? default to yes when ran first time
 	while editing:
 		print("[!] Preview of request:\n{0}".format(json_data))
-		user_input = input("[S]end request, [E]dit file, [A]bort: ")
+		user_input = input("[S]end request, [E]dit file (make sure to save!), [A]bort: ")
 		if user_input.upper() == 'S':
 			editing = 0
 			print("[+] sending post request to {0}:{1}".format(feilong_server, feilong_port))
+
+			# send the POST request
+			headers = {'content-type': 'application/json'}
+			response = requests.post(request_url, data=json.dumps(json_data), headers=headers)
+
+			# Log the event
+			event = {"last_query": request_url}
+			event["last_query_time"] = strftime("%Y-%m-%d %H:%M:%S", localtime())
+			log_event(event)
+
+			print("[+] RESPONSE: \n{0}".format(response.text))
+
+		# Edit the data, save it to the file and reload it
 		elif user_input.upper() == 'E':
 			subprocess.call([EDITOR, "request_data/" + api_name + ".json"])
+			json_file = open("request_data/" + api_name + ".json", 'r')
+			json_data = json.load(json_file)
+			json_file.close()			
+
 		elif user_input.upper() == "A":
+			editing = 0
 			print("[+] Aborting...")
-			return
+		else:
+			user_input = input("[S]end request, [E]dit file, [A]bort: ")
 
 
 
@@ -123,13 +141,14 @@ def log_event(event=None):
 		# Write back to config file
 		config_file = open("config.json", 'w')
 		config_file.write(json.dumps(config, sort_keys=True, indent=4))
-		config_file.close
+		config_file.close()
 
-# Use a dispatch table technique to
-# choose which function we will execute
-# param: user_choice: integer representing
-# the function the user wants to call
-def switch_choice(user_choice):
+
+# Main function
+if __name__ == "__main__":
+
+	# Use a dispatch table technique to
+	# choose which function we will execute	
 	table = {
 		1: send_get_request,
 		2: send_post_request,
@@ -138,20 +157,16 @@ def switch_choice(user_choice):
 		5: exit_program
 	}
 
-	# execute desired function
-	func = table.get(user_choice, lambda: "Invalid choice")
-	func()
-
-if __name__ == "__main__":
 	print(welcome_message)
 	print_menu()
 
 	while True:
 		try:
 			user_choice = int(input("[menu] > "))
+			if user_choice in range(1, len(table)+1):
+				func = table.get(user_choice, lambda: "Invalid choice")
+				func()
 		except ValueError as e:
 			print("[ERROR]: Please use a valid integer for a choice!")
 			print("[DEBUG]: error: {0}".format(e))
 			user_value = 0 # clear user value!
-			
-		switch_choice(user_choice)
